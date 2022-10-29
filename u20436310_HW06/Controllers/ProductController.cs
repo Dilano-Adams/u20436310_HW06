35138e5e9c4af3a914c56f629acc8b32cf20e627
Dilano-Adams <102999633+Dilano-Adams@users.noreply.github.com>
2022-10-29 21:14:05 +0200

    Product and Report Complete

--- u20436310_HW06/Controllers/ProductController.cs
+++ u20436310_HW06/Controllers/ProductController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using PagedList;
 using u20436310_HW06.Models;
 
 namespace u20436310_HW06.Controllers
@@ -14,26 +16,66 @@ namespace u20436310_HW06.Controllers
     {
         private BikeStoresEntities db = new BikeStoresEntities();
 
+        // GET: Brands
+        public string Brands() // Used to populate the modal dropdown. See Index View
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<brand> brands = db.brands.ToList();
+            return JsonConvert.SerializeObject(brands);
+        }
+        // GET: Catergories
+        public string Categories() // Used to populate the modal dropdown. See Index View
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<category> categories = db.categories.ToList();
+            return JsonConvert.SerializeObject(categories);
+        }
+
         // GET: Product
-        public ActionResult Index()
+        public ActionResult Index(string search,  int? page)
         {
             var products = db.products.Include(p => p.brand).Include(p => p.category);
-            return View(products.ToList());
+
+            if (search != null)
+            {
+                products = db.products.Where(x => x.product_name.Contains(search)).Include(p => p.brand).Include(p => p.category);
+            }
+
+            return View(products.ToList().ToPagedList(page ?? 1, 10));
         }
 
         // GET: Product/Details/5
         public ActionResult Details(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            product product = db.products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+            //product product = db.products.Find(id);
+            //if (product == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            //return View(product);
+
+            return PartialView(id);
+        }
+        [HttpPost]
+        public string Details(int id)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            object product = db.stocks.Where(y => y.product_id == id)
+                .Include(v => v.product)
+                .Select(p => new {
+                    product_name = p.product.product_name,
+                    model_year = p.product.model_year,
+                    list_price = p.product.list_price,
+                    shops = db.stocks.Where(s => s.product_id == id).Select(n => new { store_name = n.store.store_name, quantity = n.quantity }),
+                    brand_name = p.product.brand.brand_name,
+                    category_name = p.product.category.category_name
+            }).FirstOrDefault();
+
+            return JsonConvert.SerializeObject(product);
         }
 
         // GET: Product/Create
@@ -41,7 +83,7 @@ namespace u20436310_HW06.Controllers
         {
             ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name");
             ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name");
-            return View();
+            return PartialView();
         }
 
         // POST: Product/Create
@@ -66,18 +108,19 @@ namespace u20436310_HW06.Controllers
         // GET: Product/Edit/5
         public ActionResult Edit(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            product product = db.products.Find(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name", product.brand_id);
-            ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name", product.category_id);
-            return View(product);
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+            //product product = db.products.Find(id);
+            //if (product == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            //ViewBag.brand_id = new SelectList(db.brands, "brand_id", "brand_name", product.brand_id);
+            //ViewBag.category_id = new SelectList(db.categories, "category_id", "category_name", product.category_id);
+            //return View(product);
+            return PartialView();
         }
 
         // POST: Product/Edit/5
---
+++ u20436310_HW06/Controllers/ReportController.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using u20436310_HW06.Models;
+
+namespace u20436310_HW06.Controllers
+{
+    public class ReportController : Controller
+    {
+        private BikeStoresEntities db = new BikeStoresEntities();
+
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string chartData()
+        {
+            object orders = db.orders.Select(p => new
+            {
+                Products = db.order_items.Where(y => y.order_id == p.order_id).Select(o => new { category = o.product.category.category_name, Quantity = o.quantity, month = p.order_date.Month }).ToList<dynamic>(),
+            }).ToList();
+
+            return JsonConvert.SerializeObject(orders);
+        }
+    }
+}
